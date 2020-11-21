9ca339d300e7159d5dc7acf423374b096b73a360
Jacob Yeung <jacobyeung@berkeley.edu>
2020-11-21 02:39:16 -0800

    Big updates

--- process.py
+++ process.py
@@ -60,114 +60,115 @@ gene_id = get_train_genes(train_root)
 
 smallest_key = len(min(gene_id, key=len))
 bool_pd = np.isin(lst.columns.str[-smallest_key:], gene_id)
-train_pd = lst.T[bool_pd]
-
+train_pd = lst.T[bool_pd].T
+train_pd = train_pd.iloc[:, :1000]
 corr_pd_train, idx = cluster_corr(train_pd.corr())
+# corr_pd_train = train_pd.corr()
 corr_train = np.array(corr_pd_train)
 print(train_pd.shape)
 
-np.savez("correlation_matrix_" + status + ".npz", corr_train)
-corr_pd_train.to_csv("correlation_pd_" + status + ".csv")
-figure, ax = plt.subplots(1, 2, figsize=(56, 20))
-for i in range(len(lst)):
-    one_corr = corr_train[i, :]
-    one_corr.sort()
-    one_corr = one_corr[::-1]
-    ax[0].plot(np.arange(len(lst)), one_corr)
-sn.heatmap(corr_pd_train, xticklabels=True, yticklabels=True,
+# np.savez("correlation_matrix_" + status + ".npz", corr_train)
+# corr_pd_train.to_csv("correlation_pd_" + status + ".csv")
+figure, ax = plt.subplots(1, 2, figsize=(200, 100))
+# for i in range(len(lst)):
+#     one_corr = corr_train[i, :]
+#     one_corr.sort()
+#     one_corr = one_corr[::-1]
+#     ax[0].plot(np.arange(len(lst)), one_corr)
+sn.heatmap(corr_pd_train,
            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
 
-figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
+figure.savefig("Correlation " + status + " " + path[-2:] + " Gene.png")
 
-"""
-Valid correlation
-"""
-status = "valid_sorted"
-train_root = 'data/E003/classification/valid.csv'
-gene_id = get_train_genes(train_root)
+# """
+# Valid correlation
+# """
+# status = "valid_sorted"
+# train_root = 'data/E003/classification/valid.csv'
+# gene_id = get_train_genes(train_root)
 
-smallest_key = len(min(gene_id, key=len))
-bool_pd = np.isin(lst.columns.str[-smallest_key:], gene_id)
-train_pd = lst.T[bool_pd]
+# smallest_key = len(min(gene_id, key=len))
+# bool_pd = np.isin(lst.columns.str[-smallest_key:], gene_id)
+# train_pd = lst.T[bool_pd]
 
-corr_pd_valid = train_pd.corr().iloc[idx, :].T.iloc[idx, :]
-corr_valid = np.array(corr_pd_valid)
-print(train_pd.shape)
+# corr_pd_valid = train_pd.corr().iloc[idx, :].T.iloc[idx, :]
+# corr_valid = np.array(corr_pd_valid)
+# print(train_pd.shape)
 
-np.savez("correlation_matrix_" + status + ".npz", corr_valid)
-corr_pd_valid.to_csv("correlation_pd_" + status + ".csv")
-figure, ax = plt.subplots(1, 2, figsize=(56, 20))
-for i in range(len(lst)):
-    one_corr = corr_valid[i, :]
-    one_corr.sort()
-    one_corr = one_corr[::-1]
-    ax[0].plot(np.arange(len(lst)), one_corr)
-sn.heatmap(corr_pd_valid, xticklabels=True, yticklabels=True,
-           cmap="viridis", square=True, vmin=0.0, vmax=1.0)
+# np.savez("correlation_matrix_" + status + ".npz", corr_valid)
+# corr_pd_valid.to_csv("correlation_pd_" + status + ".csv")
+# figure, ax = plt.subplots(1, 2, figsize=(56, 20))
+# for i in range(len(lst)):
+#     one_corr = corr_valid[i, :]
+#     one_corr.sort()
+#     one_corr = one_corr[::-1]
+#     ax[0].plot(np.arange(len(lst)), one_corr)
+# sn.heatmap(corr_pd_valid, xticklabels=True, yticklabels=True,
+#            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
 
-figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
+# figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
 
 
-"""
-Test correlation
-"""
-status = "test_sorted"
-train_root = 'data/E003/classification/test.csv'
-gene_id = get_train_genes(train_root)
+# """
+# Test correlation
+# """
+# status = "test_sorted"
+# train_root = 'data/E003/classification/test.csv'
+# gene_id = get_train_genes(train_root)
 
-smallest_key = len(min(gene_id, key=len))
-bool_pd = np.isin(lst.columns.str[-smallest_key:], gene_id)
-train_pd = lst.T[bool_pd]
+# smallest_key = len(min(gene_id, key=len))
+# bool_pd = np.isin(lst.columns.str[-smallest_key:], gene_id)
+# train_pd = lst.T[bool_pd]
 
-corr_pd_test = train_pd.corr().iloc[idx, :].T.iloc[idx, :]
-corr_test = np.array(corr_pd_test)
-print(train_pd.shape)
-np.savez("correlation_matrix_" + status + ".npz", corr_test)
-corr_pd_test.to_csv("correlation_pd_" + status + ".csv")
-figure, ax = plt.subplots(1, 2, figsize=(56, 20))
-for i in range(len(lst)):
-    one_corr = corr_test[i, :]
-    one_corr.sort()
-    one_corr = one_corr[::-1]
-    ax[0].plot(np.arange(len(lst)), one_corr)
-sn.heatmap(corr_pd_test, xticklabels=True, yticklabels=True,
-           cmap="viridis", square=True, vmin=0.0, vmax=1.0)
+# corr_pd_test = train_pd.corr().iloc[idx, :].T.iloc[idx, :]
+# corr_test = np.array(corr_pd_test)
+# print(train_pd.shape)
+# np.savez("correlation_matrix_" + status + ".npz", corr_test)
+# corr_pd_test.to_csv("correlation_pd_" + status + ".csv")
+# figure, ax = plt.subplots(1, 2, figsize=(56, 20))
+# for i in range(len(lst)):
+#     one_corr = corr_test[i, :]
+#     one_corr.sort()
+#     one_corr = one_corr[::-1]
+#     ax[0].plot(np.arange(len(lst)), one_corr)
+# sn.heatmap(corr_pd_test, xticklabels=True, yticklabels=True,
+#            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
 
-figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
+# figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
 
-"""
-Overall correlation
-"""
-train_pd = lst.T
-status = "overall_sorted"
-corr_pd = train_pd.corr().iloc[idx, :].T.iloc[idx, :]
-corr = np.array(corr_pd)
-print(train_pd.shape)
+# """
+# Overall correlation
+# """
+# train_pd = lst.T
+# status = "overall_sorted"
+# corr_pd = train_pd.corr().iloc[idx, :].T.iloc[idx, :]
+# corr = np.array(corr_pd)
+# print(train_pd.shape)
 
-np.savez("correlation_matrix_" + status + ".npz", corr)
-corr_pd.to_csv("correlation_pd_" + status + ".csv")
-figure, ax = plt.subplots(1, 2, figsize=(56, 20))
-for i in range(len(lst)):
-    one_corr = corr[i, :]
-    one_corr.sort()
-    one_corr = one_corr[::-1]
-    ax[0].plot(np.arange(len(lst)), one_corr)
-sn.heatmap(corr_pd, xticklabels=True, yticklabels=True,
-           cmap="viridis", square=True, vmin=0.0, vmax=1.0)
+# np.savez("correlation_matrix_" + status + ".npz", corr)
+# corr_pd.to_csv("correlation_pd_" + status + ".csv")
+# figure, ax = plt.subplots(1, 2, figsize=(56, 20))
+# for i in range(len(lst)):
+#     one_corr = corr[i, :]
+#     one_corr.sort()
+#     one_corr = one_corr[::-1]
+#     ax[0].plot(np.arange(len(lst)), one_corr)
+# sn.heatmap(corr_pd, xticklabels=True, yticklabels=True,
+#            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
 
-figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
+# figure.savefig("Correlation " + status + " " + path[-2:] + ".png")
 
-figure, ax = plt.subplots(figsize=(24, 20))
-sn.heatmap((corr_pd_test - corr_pd_train).abs(),
-           cmap="viridis", square=True, vmin=0.0, vmax=1.0)
-figure.savefig("Correlation " + "test and train" + " " + path[-2:] + ".png")
+# figure, ax = plt.subplots(figsize=(24, 20))
+# sn.heatmap((corr_pd_test - corr_pd_train).abs(),
+#            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
+# figure.savefig("Correlation " + "test and train" + " " + path[-2:] + ".png")
 
-figure, ax = plt.subplots(figsize=(24, 20))
-sn.heatmap((corr_pd_valid - corr_pd_train).abs(),
-           cmap="viridis", square=True, vmin=0.0, vmax=1.0)
-figure.savefig("Correlation " + "valid and train" + " " + path[-2:] + ".png")
+# figure, ax = plt.subplots(figsize=(24, 20))
+# sn.heatmap((corr_pd_valid - corr_pd_train).abs(),
+#            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
+# figure.savefig("Correlation " + "valid and train" + " " + path[-2:] + ".png")
 
-figure, ax = plt.subplots(figsize=(24, 20))
-sn.heatmap((corr_pd_valid - corr_pd_test).abs(),
-           cmap="viridis", square=True, vmin=0.0, vmax=1.0)
-figure.savefig("Correlation " + "valid and test" + " " + path[-2:] + ".png")
+# figure, ax = plt.subplots(figsize=(24, 20))
+# sn.heatmap((corr_pd_valid - corr_pd_test).abs(),
+#            cmap="viridis", square=True, vmin=0.0, vmax=1.0)
+# figure.savefig("Correlation " + "valid and test" + " " + path[-2:] + ".png")
